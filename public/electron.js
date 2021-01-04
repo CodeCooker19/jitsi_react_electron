78ed8c7947f65a712a418b33a9f9c0cb23083603
Ahmed B <kinglancer0214@protonmail.com>
2021-01-04 19:12:18 +0100

    Completed tray icon function.

--- public/electron.js
+++ public/electron.js
@@ -4,11 +4,13 @@ const { app, BrowserWindow, Menu, Tray } = electron;
 const URL = require('url');
 const path = require('path');
 const isDev = require('electron-is-dev');
+const nativeImage = require('electron').nativeImage
 
 let mainWindow = null;
 let tray = null;
 
 app.on('ready', () => {
+
   // create a new `splash`-Window 
   splash = new BrowserWindow({
     transparent: true,
@@ -33,14 +35,23 @@ app.on('ready', () => {
   mainWindow.removeMenu();
   // mainWindow.webContents.openDevTools();
   mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`);
-  
-  mainWindow.on('closed', function () {
-    mainWindow = null
-  })
+
+  mainWindow.on('minimize', function (event) {
+    event.preventDefault();
+    mainWindow.minimize();
+  });
+
+  mainWindow.on('close', function (event) {
+
+    event.preventDefault();
+    mainWindow.hide();
+    return false;
+  });
+
   mainWindow.on('page-title-updated', function (e) {
     e.preventDefault()
   });
-  
+
   // if main window is ready to show, then destroy the splash window and show up the main window
   mainWindow.once('ready-to-show', () => {
     splash.destroy();
@@ -49,25 +60,32 @@ app.on('ready', () => {
   });
 
   //Tray icon
-  tray = new Tray(path.join(__dirname, '/images/production_mark.ico'));
-
-  if (process.platform === 'win32') {
-    tray.on('click', tray.popUpContextMenu);
-  }
-
+  const trayIcon = path.join(__dirname, 'images/production_mark.ico');
+  const nimage = nativeImage.createFromPath(trayIcon);
+  tray = new Tray(nimage);
   const menu = Menu.buildFromTemplate([
     {
-      label: 'Quit',
-      click() { app.quit(); }
+      label: 'Quit', click: function () {
+        mainWindow.destroy();
+        app.quit();
+      }
     }
   ]);
-
   tray.setToolTip('Jitsi-Meet-Demo');
   tray.setContextMenu(menu);
+  tray.on('click', (event, bounds, position) => {
+    if (mainWindow.isVisible()) {
+      mainWindow.hide();
+    } else {
+      mainWindow.show();
+    }
+  });
 });
 
-app.on('window-all-closed', function () {
+app.on('window-all-closed', function (event) {
+  console.log(">>>>window-all-Closed");
   if (process.platform !== 'darwin') {
-    app.quit()
+    console.log(">>>>window-all-Closed, darwin");
+    app.quit();
   }
 });
